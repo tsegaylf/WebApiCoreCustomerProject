4a6742cf161573e747c8a8460b9d28c59858a631
Lidya Tsegay <lidyatsegay@outlook.com>
2019-09-13 11:45:33 -0400

    Created 2 migrations 1)Cust Database and table 2)Update to Cust Class Properties

---
+++ WebApiCoreCustomerProject/Migrations/20190913150533_Initialization.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApiCoreCustomerProject.Migrations
+{
+    public partial class Initialization : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Sales = table.Column<double>(nullable: false),
+                    Active = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
--- WebApiCoreCustomerProject/Models/Customer.cs
+++ WebApiCoreCustomerProject/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,12 @@ namespace WebApiCoreCustomerProject.Models {
 
     public class Customer {
         public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
         public double Sales { get; set; }
         public bool Active { get; set; }
+
 
         public Customer() {
 
